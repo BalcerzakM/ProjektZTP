c10b839efd7bf953c091065f6ab6e6cc49173a9b
BalcerzakM <mcj.balcerzak@gmail.com>
2025-12-09 18:48:28 +0100

    Factory stoi pod znakiem zapytania, nowy tryb

--- src/ConnectMode.java
+++ src/ConnectMode.java
@@ -1,20 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;//jeszcze nie wiem gdzie go zostawie
 
-public class FlashCardMode implements LearningMode{
+public class ConnectMode implements LearningMode{
     @Override
     public void start(WordSet wordSet) {
         Scanner scanner = new Scanner(System.in);
         QuestionFactory factory = new FlashCardQuestionFactory();
         System.out.println("*******************************");
-        System.out.println("          Tryb Fiszek!");
+        System.out.println("          Tryb Łączenia!");
         System.out.println("*******************************");
         System.out.println();
+
+        List<String> tab1 = new ArrayList<String>();
+        List<String> tab2 = new ArrayList<String>();
+
         for (Word word : wordSet.getWords()) {
+
+            tab1.add(word.getSource());
+            tab2.add(word.getTarget());
+
+            //Question q = factory.createQuestion(word, wordSet);
             System.out.println("*******************************");
             System.out.println();
-            System.out.println("            "+word.getSource());
+            System.out.println("            "+q.getPrompt());
             String line = scanner.nextLine();
-            System.out.println("            "+word.getTarget());
+            System.out.println("            "+q.getCorrectAnswer());
             line = scanner.nextLine();
             System.out.println("*******************************");
         }
