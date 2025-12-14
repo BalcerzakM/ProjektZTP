604fd4b86ec471ed9ad7055b1fdf509d2b6e3126
pborysq <pborysq@gmail.com>
2025-12-14 01:07:15 +0100

    Connector do baz danych jako singleton, wywalone LearningApp

---
+++ src/app/Connector.java
@@ -0,0 +1,38 @@
+package app;
+
+import models.Word;
+import models.WordSet;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Connector {
+    private static Connector instance;
+
+    private Connector() {
+    }
+
+    public static Connector getInstance() {
+        if (instance == null) {
+            instance = new Connector();
+        }
+        return instance;
+    }
+
+    public WordSet readWordSetFromFile(String fileName) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(fileName));
+        String difficulty = scanner.nextLine().strip();
+        WordSet wordSet = new WordSet(fileName, new ArrayList<>(), difficulty);
+        while (scanner.hasNextLine()) {
+            String[] line = scanner.nextLine().split("-");
+            String source = line[0].strip();
+            String target = line[1].strip();
+            Word word = new Word(source, target);
+            wordSet.addWord(word);
+        }
+        scanner.close();
+        return wordSet;
+    }
+}
