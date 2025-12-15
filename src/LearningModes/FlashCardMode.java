0a0b0a054c4520a75dc457eeb10918049008498c
BalcerzakM <mcj.balcerzak@gmail.com>
2025-12-15 11:12:47 +0100

    Poprawki

--- src/LearningModes/FlashCardMode.java
+++ src/LearningModes/FlashCardMode.java
@@ -3,8 +3,6 @@ package LearningModes;
 import models.LearningSession;
 import models.Word;
 import models.WordSet;
-import questions.FlashCardQuestionFactory;
-import questions.QuestionFactory;
 
 
 import java.util.Scanner;//jeszcze nie wiem gdzie go zostawie
@@ -13,7 +11,6 @@ public class FlashCardMode implements LearningMode {
     @Override
     public void start(WordSet wordSet, LearningSession learningSession) {
         Scanner scanner = new Scanner(System.in);
-        QuestionFactory factory = new FlashCardQuestionFactory();
         System.out.println("*******************************");
         System.out.println("          Tryb Fiszek!");
         System.out.println("*******************************");
