10a41dc15fedc7626c97f62c2ff3361a8d238c0f
BalcerzakM <mcj.balcerzak@gmail.com>
2026-01-09 14:19:26 +0100

    Statystyki wstępnie

--- src/controllers/LearningSessionController.java
+++ src/controllers/LearningSessionController.java
@@ -30,58 +30,71 @@ public class LearningSessionController implements Controller {
         LearningSessionPanel panel = new LearningSessionPanel();
 
         panel.onFlashCard(() -> {
+            SessionStatistics stats = new SessionStatistics();
+            model.registerObserver(stats);
             new FlashCardController(
                     frame,
                     model,
                     context.getCurrentWordSet(),
                     () -> {
+                        model.unregisterObserver(stats);
                         JOptionPane.showMessageDialog(
-                                frame,"statystyki",
-                                //stats.showStatistics(),
+                                frame,
+                                stats.showStatistics(),
                                 "Session summary",
                                 JOptionPane.INFORMATION_MESSAGE
-                        );//Statystyki na przykład , jak nie tu to wywalić. Albo dodać metode, która tu by się wykonywała
+                        );
                         frame.setView(panel,"MENU");
                     }
             ).start();
         });
 
-        //panel.onConnect(() -> startMode(new ConnectMode(context.getCurrentWordSet()), context));
         panel.onConnect(() -> {
+            SessionStatistics stats = new SessionStatistics();
+            model.registerObserver(stats);
                     new ConnectController(
                             frame,
                             model,
                             context.getCurrentWordSet()
                     ).start();
                 });
+
         panel.onMillionaire(() -> {
+            SessionStatistics stats = new SessionStatistics();
+            model.registerObserver(stats);
             new MillionaireController(
                     frame,
                     model,
                     context.getCurrentWordSet(),
                     4,
                     () -> {
+                        model.unregisterObserver(stats);
                         JOptionPane.showMessageDialog(
-                                frame,"statystyki",
-                                //stats.showStatistics(),
+                                frame,
+                                stats.showStatistics(),
                                 "Session summary",
                                 JOptionPane.INFORMATION_MESSAGE
-                        );//Statystyki na przykład , jak nie tu to wywalić. Albo dodać metode, która tu by się wykonywała
+                        );
                         frame.setView(panel,"MENU");
                     }
                     ).start();
         });
-        panel.onTyping(() ->{ new TypingController(
+
+        panel.onTyping(() ->{
+            SessionStatistics stats = new SessionStatistics();
+            model.registerObserver(stats);
+            new TypingController(
                 frame,
                 model,
                 context.getCurrentWordSet(), 4,
                 () -> {
+                    model.unregisterObserver(stats);
                     JOptionPane.showMessageDialog(
-                            frame,"statystyki",
-                            //stats.showStatistics(),
+                            frame,
+                            stats.showStatistics(),
                             "Session summary",
                             JOptionPane.INFORMATION_MESSAGE
-                    );//Statystyki na przykład. dodać metode, która tu by się wykonywała
+                    );
                     frame.setView(panel,"MENU");
                 }
         ).start();});
@@ -92,22 +105,6 @@ public class LearningSessionController implements Controller {
         //return AppState.LearningSession; // GUI steruje dalej
     }
 
-    private void startMode(LearningMode mode, AppContext context) {
-        SessionStatistics stats = new SessionStatistics();
-        model.registerObserver(stats);
 
-        model.setMode(mode);
-        model.getMode().start(context.getCurrentWordSet(), model);
-
-        model.unregisterObserver(stats);
-
-        JOptionPane.showMessageDialog(
-                frame,
-                stats.showStatistics(),
-                "Session summary",
-                JOptionPane.INFORMATION_MESSAGE
-        );
-        //STATYSTYKI albo w osobnej metodzie albo w każdym z kontrolerów mode
-    }
 }
 
